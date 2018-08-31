ab0a521555d15c53ef289a1ce46114b521b1bc9c
dedelman04 <42451900+dedelman04@users.noreply.github.com>
2018-08-31 08:23:22 -0500

    Add files via upload

--- annotate boxplots.R
+++ annotate boxplots.R
@@ -1,11 +1,11 @@
-dataset <- dataset %>% group_by(ServerName) %>% filter(n() >= med) %>% 
-  mutate(Success = Success, AttemptDur = AttemptDur) %>% ungroup()
+#Filter top 50%
+dataset <- dataset %>% group_by(ServerName) %>% filter(n() >= med) %>% ungroup()
 
-counts <- dataset %>% group_by(ServerName) %>% tally()
+counts <- dataset %>% group_by(ServerName) %>% tally() #tally is short hand for summarize and getting n()
 
 dataset %>% ggplot(aes(x=ServerName, y=AttemptDur))+
   geom_boxplot()+
   geom_point(aes(color = Success))+ scale_color_manual(values=c("Green", "Red"))+
   theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1))+
-  geom_text(data = counts, aes(label=paste("n =", n), y=0, angle = 90), size = 2.5)+
+  geom_text(data = counts, aes(label=paste("n =", n), y=0, angle = 90), size = 2.5)+ #annotation layer
   ylim(-5, max+5)
