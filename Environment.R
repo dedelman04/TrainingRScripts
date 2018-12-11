2e0f88cd6834b40b423cb468c6382129a8aa0403
David Edelman <dedelman@cameronlng.com>
2018-12-11 13:56:01 -0600

    added plot_cond_prob to Environment updates notes elsewhere on 2nd viewing

--- Environment.R
+++ Environment.R
@@ -31,3 +31,11 @@ library(lpSolve)
 library(rvest)
 library(caret)
 library(pdftools)
+
+plot_cond_prob <- function(p_hat=NULL){ 
+  tmp <- mnist_27$true_p 
+  if(!is.null(p_hat)){ tmp <- mutate(tmp, p=p_hat) } 
+  tmp %>% ggplot(aes(x_1, x_2, z=p, fill=p)) + 
+    geom_raster(show.legend = FALSE) + 
+    scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) + 
+    stat_contour(breaks=c(0.5),color="black") }
