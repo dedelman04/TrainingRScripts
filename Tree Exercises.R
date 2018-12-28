8302845885c757ad0d1edc9fd933372912900129
David Edelman <dedelman04@gmail.com>
2018-12-27 18:05:19 -0600

    Tree/Forest Exercises

---
+++ Tree Exercises.R
@@ -0,0 +1,42 @@
+#Trees & Forests exercises
+
+library(rpart)
+n <- 1000
+sigma <- 0.25
+x <- rnorm(n, 0, 1)
+y <- 0.75 * x + rnorm(n, 0, sigma)
+dat <- data.frame(x = x, y = y)
+
+#Q1 - fit tree model to dat
+fit <- rpart(y ~ ., data = dat)
+
+#Q2 - plot the tree
+plot(fit, margin=0.1)
+text(fit, cex=.75)
+
+#Q3 - scatterplot of y vs x
+dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col=2)
+
+#Q4 - now do random forest
+library(randomForest)
+fit <- randomForest(y ~ x, data = dat)
+  dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col = 2)
+
+#Q5 - look for convergence of forest
+plot(fit)
+
+#Q6 - set parameters on forest to max 25 nodes, node size of 50
+fit <- randomForest(y ~ x, data = dat, nodesize = 50, maxnodes = 25)
+  dat %>% 
+  mutate(y_hat = predict(fit)) %>% 
+  ggplot() +
+  geom_point(aes(x, y)) +
+  geom_step(aes(x, y_hat), col = 2)
